8e5b2eaab575acc41b95212bc4e491e98fde836e
Adrian Gomez <adriangomez2099@gmail.com>
2020-07-14 15:15:46 -0600

    Archivos de Trampas
    Archivos de las trampas taser,spikes,fire

---
+++ Spirit Tower/Assets/Scripts/Traps/Fire_Trap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Trap : MonoBehaviour
+{
+    private Collider2D FireCannon;
+    private AudioSource FireSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FireCannon = this.GetComponent<Collider2D>();
+        FireSound = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FireON()
+    {
+        FireCannon.enabled = true;
+        FireSound.Play();
+    }
+
+    private void FireOFF()
+    {
+        FireCannon.enabled = false;
+        FireSound.Stop();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+}
---
+++ Spirit Tower/Assets/Scripts/Traps/Spike_Trap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike_Trap : MonoBehaviour
+{
+    private Collider2D SpikeTrap;
+    private AudioSource SpikeSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpikeTrap = this.GetComponent<Collider2D>();
+        SpikeSound = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FireON()
+    {
+        SpikeTrap.enabled = true;
+        SpikeSound.Play();
+    }
+
+    private void FireOFF()
+    {
+        SpikeTrap.enabled = false;
+        SpikeSound.Stop();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
