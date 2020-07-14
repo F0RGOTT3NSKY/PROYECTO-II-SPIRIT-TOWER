086843d28548fda3ab6633b9fa470f000da1a4fb
Dennis <alejimenezc@hotmail.com>
2020-07-13 19:05:26 -0600

    Recibir Daño y Bloquear Ataques

--- Spirit Tower/Assets/CombatScript.cs
+++ Spirit Tower/Assets/CombatScript.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class CombatScript : MonoBehaviour
 {
+    public int MaxHP = 100;
+    int CurrentHP;
+    public bool isBlocking = false;
     public Animator animator;
     public Transform AttackHitbox;
     public LayerMask enemies;
     public float RangoA = 0.5f;
     public float cadenciaAB = 2f;
     public float nextAB = 0f;
+
+
     void Update()
     {
-        if(Time.time >= nextAB)
+        if (Time.time >= nextAB)
         {
             if (Input.GetKeyDown(KeyCode.V))
             {
@@ -24,23 +30,67 @@ public class CombatScript : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.C))
             {
                 Block();
+                isBlocking = true;
                 nextAB = Time.time + 1f / cadenciaAB;
 
             }
+            else if (Input.GetKeyUp(KeyCode.C))
+            {
+                isBlocking = false;
+            }
         }
     }
+
+
     void Attack()
     {
         animator.SetTrigger("Attack");
         Collider2D[] golpearE = Physics2D.OverlapCircleAll(AttackHitbox.position, RangoA, enemies);
-        foreach(Collider2D enemy in golpearE)
+        foreach (Collider2D enemy in golpearE)
         {
             Debug.Log("Its a hit");
         }
     }
 
+
+    void OnDrawGizmosSelected()
+    {
+        if (AttackHitbox == null)
+            return;
+        Gizmos.DrawWireSphere(AttackHitbox.position, RangoA);
+    }
+
+
     void Block()
     {
         animator.SetTrigger("Block");
+    }
+
+
+    public void RecieveDamage(int damage)
+    {
+        if(isBlocking == false)
+        {
+            CurrentHP -= damage;
+        }
+        else
+        {
+            CurrentHP = CurrentHP;
+        }
+
+        animator.SetTrigger("Hurt");
+
+        if(CurrentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+
+    public void Die()
+    {
+        Debug.Log("Haz muerto");
+
+        animator.SetBool("Dead", true);
     }
 }
