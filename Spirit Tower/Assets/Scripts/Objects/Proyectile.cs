0dbf023b79c59b6c89d27526180804d03fe2911f
Marco <marco99eu@gmail.com>
2020-07-21 09:04:38 -0600

    Lvl 1 - 4 completed, Red need fixes

---
+++ Spirit Tower/Assets/Scripts/Objects/Proyectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectile : MonoBehaviour
+{
+    public float Velocity;
+    public Vector2 DirectionToMove;
+    public float LifeTime;
+    private float LifeTimeSec;
+    public Rigidbody2D myrigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myrigidbody2D = GetComponent<Rigidbody2D>();
+        LifeTimeSec = LifeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LifeTimeSec -= Time.deltaTime;
+        if(LifeTimeSec <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Fire(Vector2 InitialDir)
+    {
+        myrigidbody2D.velocity = InitialDir * Velocity;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
