43d9f9673a22d8d5d3b222bd5d2b9c59474caf07
Dennis <alejimenezc@hotmail.com>
2020-07-20 23:51:27 -0600

    Transitions

---
+++ Spirit Tower/Assets/Scripts/GameRelated/SceneTransitions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger){
+            Debug.Log("Choque");
+            SceneManager.LoadScene(SceneToLoad);
+
+        }
+    }
+
+}
